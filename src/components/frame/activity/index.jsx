ca06770876c923e4e6ced6ffa1820f60efb431f8
maybenotpanda <maybenotpanda@gmail.com>
2025-10-28 12:56:18 +0700

    feat-activity: add fn

--- src/components/frame/activity/index.jsx
+++ src/components/frame/activity/index.jsx
@@ -5,22 +5,25 @@ import React from 'react'
 import { LuClock8, LuClock6 } from 'react-icons/lu'
 import { PiClockClockwiseBold } from 'react-icons/pi'
 
-const Activity = () => {
+const Activity = (props) => {
+	// ! props
+	const { checkIn, working, checkOut } = props
+	
 	return (
 		<div className="flex justify-between">
 			<div className="grid gap-1 justify-items-center">
 				<LuClock8 className="text-4xl text-primary" />
-				<h6 className="font-semibold">08:30</h6>
+				<h6 className="font-semibold">{checkIn}</h6>
 				<p>Check In</p>
 			</div>
 			<div className="grid gap-1 justify-items-center">
 				<PiClockClockwiseBold className="text-4xl text-primary" />
-				<h6 className="font-semibold text-primary">03:00:00</h6>
+				<h6 className="font-semibold text-primary">{working}</h6>
 				<p>Working Hours</p>
 			</div>
 			<div className="grid gap-1 justify-items-center">
 				<LuClock6 className="text-4xl text-primary" />
-				<h6 className="font-semibold">--:--</h6>
+				<h6 className="font-semibold">{checkOut}</h6>
 				<p>Check Out</p>
 			</div>
 		</div>
