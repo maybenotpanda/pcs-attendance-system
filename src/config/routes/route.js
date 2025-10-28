9ce84f1756e0cba46526537a8183a8c2dc29bd7e
maybenotpanda <maybenotpanda@gmail.com>
2025-10-28 12:07:46 +0700

    feat-route: add new pages

--- src/config/routes/route.js
+++ src/config/routes/route.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom'
 // ** Pages Imports
 import HomePage from 'pages/home'
 import Page404 from 'pages/404'
+import Notifications from 'pages/notifications'
 
 // ** Sub-Route Imports
 
@@ -12,6 +13,7 @@ const RoutePage = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />} />
+			<Route path="/notifications" element={<Notifications />} />
 			<Route path="*" element={<Page404 />} />
 		</Routes>
 	)
