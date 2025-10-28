9a6d7bac740a42cb850a67aeaf602d3feaffdb59
maybenotpanda <maybenotpanda@gmail.com>
2025-10-28 11:53:14 +0700

    feat-home: create activity on frame

---
+++ src/components/frame/activity/index.jsx
@@ -0,0 +1,30 @@
+// ** React Imports
+import React from 'react'
+
+// ** Icon Imports
+import { LuClock8, LuClock6 } from 'react-icons/lu'
+import { PiClockClockwiseBold } from 'react-icons/pi'
+
+const Activity = () => {
+	return (
+		<div className="flex justify-between">
+			<div className="grid gap-1 justify-items-center">
+				<LuClock8 className="text-4xl text-primary" />
+				<h6 className="font-semibold">08:30</h6>
+				<p>Check In</p>
+			</div>
+			<div className="grid gap-1 justify-items-center">
+				<PiClockClockwiseBold className="text-4xl text-primary" />
+				<h6 className="font-semibold text-primary">03:00:00</h6>
+				<p>Working Hours</p>
+			</div>
+			<div className="grid gap-1 justify-items-center">
+				<LuClock6 className="text-4xl text-primary" />
+				<h6 className="font-semibold">--:--</h6>
+				<p>Check Out</p>
+			</div>
+		</div>
+	)
+}
+
+export default Activity
