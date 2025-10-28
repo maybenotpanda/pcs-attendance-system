7a443987ca755a57c20a572ecd99ee45cc0a122f
maybenotpanda <maybenotpanda@gmail.com>
2025-10-28 12:09:10 +0700

    feat-home: update styles & layouts with frame

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,29 +2,31 @@
 import React from 'react'
 
 // ** Frame Imports
-import flower1 from 'assets/images/flower1.png'
-import flower2 from 'assets/images/flower2.png'
-// import Frame from 'components/frame/home'
+import Layouts from 'components/frame/layouts'
+import Activity from 'components/frame/activity'
+import NewsCarousel from 'components/frame/news-carousels'
+import UserList from 'components/frame/user-list'
+
+// ** Elements Imports
+import CardProfile from 'components/elements/card-profile'
+import Greeting from 'components/elements/greeting'
+
+// ** Assets Imports
+import News from 'assets/data/news.json'
 
 const HomePage = () => {
 	return (
-		<div className="h-screen flex justify-center items-center bg-[#E1E2E2]">
-			<div className="grid gap-4 px-5">
-				<h4 className="text-center text-[#545454]">THE WEDDING OF</h4>
-				<h1 className="text-center text-header text-[#865D3D]">MW & Reza</h1>
-				<h4 className="text-center text-[#545454]">Lokasi Acara : </h4>
-				<div className="grid gap-5">
-					<button className="bg-[#0D2C46] text-white py-2 hover:bg-[#0D2C46]">Lokasi Akad</button>
-					<button className="bg-[#0D2C46] text-white py-2 hover:bg-[#0D2C46]">Lokasi Resepsi</button>
-					<button className="bg-[#0D2C46] text-white py-2 hover:bg-[#0D2C46]">Lokasi Unduh Mantu</button>
-				</div>
-				   <div className="absolute bottom-0 left-0  image-left">
-        <img className="w-36 bg-cover" src={flower1} alt="flower 1" />
-      </div>
-      <div className="absolute bottom-0 right-0 image-right">
-        <img className="w-36 bg-cover" src={flower2} alt="Flower 2" />
-      </div>
-			</div>
+		<div className="w-screen flex justify-center items-start">
+			<Layouts>
+				<Greeting />
+				<CardProfile />
+				<h5 className="font-semibold">Today's Activity</h5>
+				<Activity />
+				<h5 className="font-semibold">PCS News</h5>
+				<NewsCarousel data={News} />
+				<h5 className="font-semibold">Online</h5>
+				<UserList />
+			</Layouts>
 		</div>
 	)
 }
