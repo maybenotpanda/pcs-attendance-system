edcf8b04cfbc7b637f440925eb79d7bf721e4eae
maybenotpanda <maybenotpanda@gmail.com>
2025-10-28 10:09:31 +0700

    feat-notification: create new frame

---
+++ src/components/frame/message-list/index.jsx
@@ -0,0 +1,35 @@
+// ** React Imports
+import React from 'react'
+
+// ** Utils Imports
+import { Each } from 'config/utils/each'
+
+// ** Fragments Imports
+import CardMessage from 'components/fragments/card-message'
+
+const MessageList = (props) => {
+	// ! props
+	const { data } = props
+
+	console.log(data)
+	return (
+		<div className="grid">
+			<Each
+				of={data}
+				render={(item) => (
+					<CardMessage
+						key={item.id}
+						title={item.title}
+						date={item.date}
+						status={item.status}
+						type={item.type}
+						description={item.description}
+						read={item.read}
+					/>
+				)}
+			/>
+		</div>
+	)
+}
+
+export default MessageList
